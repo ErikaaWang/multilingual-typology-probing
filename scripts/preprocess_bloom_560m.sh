b693ca34bc090928c6565029660db1cf840ee26b
Hetong Wang <s2308470@buysse.inf.ed.ac.uk>
2023-05-27 12:07:04 +0100

    shell script updated

--- scripts/preprocess_bloom_560m.sh
+++ scripts/preprocess_bloom_560m.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for CORPUS in $(cat scripts/languages_bloom.lst); do
   echo "python preprocess_treebank.py $CORPUS --bloom bloom-560m" --use-gpu
   python preprocess_treebank.py $CORPUS --bloom bloom-560m --use-gpu
