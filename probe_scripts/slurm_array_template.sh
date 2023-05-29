e063845905725a7fe9db0174bfa9c4a898e18886
Hetong Wang <s2308470@buysse.inf.ed.ac.uk>
2023-05-29 12:03:07 +0100

    probe_scripts

--- probe_scripts/slurm_array_template.sh
+++ probe_scripts/slurm_array_template.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 USER=s2308470
-COMMAND="`sed \"${SLURM_ARRAY_TASK_ID}q;d\" $1`
+COMMAND="`sed \"${SLURM_ARRAY_TASK_ID}q;d\" $1`"
 
 source /home/${USER}/.bashrc
 source activate multilingual-typology-probing
