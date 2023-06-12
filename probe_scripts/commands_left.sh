77243d77c16d564b1b02aad7a4f64286e81ab433
Hetong Wang <s2308470@athos.inf.ed.ac.uk>
2023-06-12 06:33:48 +0100

    probing checkpoints

--- probe_scripts/commands_left.sh
+++ probe_scripts/commands_left.sh
@@ -5,5 +5,5 @@ USER=s2308470
 source /home/${USER}/.bashrc
 source activate multilingual-typology-probing
 
-echo "python run.py --language UD_Catalan --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-560m-intermediate-global_step1000 greedy --selection-size 50 --selection-criterion mi"
-python run.py --language UD_Catalan --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-560m-intermediate-global_step1000 greedy --selection-size 50 --selection-criterion mi
+echo "python run.py --language UD_Arabic --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-560m-intermediate-global_step200000 greedy --selection-size 50 --selection-criterion mi"
+python run.py --language UD_Arabic --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-560m-intermediate-global_step200000 greedy --selection-size 50 --selection-criterion mi
