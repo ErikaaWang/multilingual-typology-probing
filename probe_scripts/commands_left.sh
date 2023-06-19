cfeac532d92486fa8f9422a7f593f8be8e92d35f
Hetong Wang <s2308470@uhtred.inf.ed.ac.uk>
2023-06-19 12:12:48 +0100

    Number bloom-1b1 results

--- probe_scripts/commands_left.sh
+++ probe_scripts/commands_left.sh
@@ -5,5 +5,5 @@ USER=s2308470
 source /home/${USER}/.bashrc
 source activate multilingual-typology-probing
 
-echo "python run.py --language UD_Spanish --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-1b1-intermediate-global_step200000 greedy --selection-size 50 --selection-criterion mi"
-python run.py --language UD_Spanish --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-1b1-intermediate-global_step200000 greedy --selection-size 50 --selection-criterion mi
+echo "python run.py --language UD_Spanish --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-1b1 greedy --selection-size 50 --selection-criterion mi"
+python run.py --language UD_Spanish --experiment-name inter-layer-13 --attribute Number --trainer poisson --gpu --embedding bloom-1b1 greedy --selection-size 50 --selection-criterion mi
