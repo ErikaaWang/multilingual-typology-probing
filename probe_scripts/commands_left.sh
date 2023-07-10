b351cddbb88b33555b2b5305a0045ce1ddc35cd8
Hetong Wang <s2308470@glenn.inf.ed.ac.uk>
2023-07-10 15:55:31 +0100

    test script for eddie

--- probe_scripts/commands_left.sh
+++ probe_scripts/commands_left.sh
@@ -5,5 +5,5 @@ USER=s2308470
 source /home/${USER}/.bashrc
 source activate multilingual-typology-probing
 
-echo "python run.py --language UD_Spanish --experiment-name inter-layer-13 --attribute POS --trainer poisson --gpu --embedding bloom-1b1-intermediate-global_step100000 greedy --selection-size 50 --selection-criterion mi"
-python run.py --language UD_Spanish --experiment-name inter-layer-13 --attribute POS --trainer poisson --gpu --embedding bloom-1b1-intermediate-global_step100000 greedy --selection-size 50 --selection-criterion mi
+echo "python preprocess_treebank.py UD_English --bloom bloom-1b7 --use-gpu"
+python preprocess_treebank.py UD_English --bloom bloom-1b7 --use-gpu
